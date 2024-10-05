f4b2f9464d7ff15e6136929739857001d33fd117
Abhishek Arya <abhishekparmar2306@gmail.com>
2024-10-06 01:38:01 +0530

    add changes

--- frontend/src/component/CarDetails.jsx
+++ frontend/src/component/CarDetails.jsx
@@ -92,7 +92,7 @@ export default function CarDetails() {
   return (
     <div className="bg-gray-100 min-h-screen p-6">
       <div className="flex justify-center my-12 bg-white rounded-lg">
-        <img src={car?.imgUrl} alt="Car Main" className="w-1/3 h-1/3 rounded-lg" />
+        <img src={car?.imgUrl} alt="Car Main" className="md:w-1/3 md:h-1/3 rounded-lg" />
       </div>
 
       <div className="flex flex-col lg:flex-row gap-8">
